206e2f3842e377aedb65186c8137c2cdb118f860
jessekokanovic <41411258+jessekokanovic@users.noreply.github.com>
2018-09-11 16:07:17 +1000

    Correctly display km driven if distance to drive exceeds fuel.

--- prac_06/car_simulator.py
+++ prac_06/car_simulator.py
@@ -11,10 +11,9 @@ def main():
     print("Let's Drive!")
     car_name = input("Enter your car name: ")
     user_car = Car(car_name, 100)
-
     print(user_car)
     print(MENU)
-    menu_selection = determine_valid_user_choice(input("Enter your choice: ").lower())
+    menu_selection = verify_user_choice(input("Enter your choice: ").lower())
     while menu_selection != 'q':
         if menu_selection == 'd':
             # Drive functionality
@@ -22,15 +21,17 @@ def main():
             while distance_to_drive < 0:
                 print("Distance must be >=0")
                 distance_to_drive = int(input("How many km do you wish to drive? "))
-            user_car.drive(distance_to_drive)
-            if user_car.fuel == 0:
+            if distance_to_drive >= user_car.fuel:
                 empty_tank_string = " and ran out of fuel"
+                distance_to_drive = user_car.fuel
+                user_car.drive(distance_to_drive)
             else:
                 empty_tank_string = ""
+                user_car.drive(distance_to_drive)
             print("The car drove {}km{}.\n".format(distance_to_drive, empty_tank_string))
             print(user_car)
             print(MENU)
-            menu_selection = determine_valid_user_choice(input("Enter your choice: ").lower())
+            menu_selection = verify_user_choice(input("Enter your choice: ").lower())
         else:
             # Refuel functionality
             amount_to_refuel = int(input("How many units of fuel do you want to add to the car?"))
@@ -41,11 +42,11 @@ def main():
             print("Added {} units of fuel.\n".format(amount_to_refuel))
             print(user_car)
             print(MENU)
-            menu_selection = determine_valid_user_choice(input("Enter your choice: ").lower())
+            menu_selection = verify_user_choice(input("Enter your choice: ").lower())
     print("\nGoodbye {}'s driver".format(user_car.name))
 
 
-def determine_valid_user_choice(choice):
+def verify_user_choice(choice):
     while choice not in MENU_OPTIONS:
         print("Invalid Choice")
         print(MENU)
