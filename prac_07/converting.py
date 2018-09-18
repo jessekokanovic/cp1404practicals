ca2593ac4075a89fc81ba20278c85822f02ef0ea
jessekokanovic <41411258+jessekokanovic@users.noreply.github.com>
2018-09-18 15:46:47 +1000

    Initial commit for converting program.

---
+++ prac_07/converting.py
@@ -0,0 +1,33 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+MILES_TO_KM = 1.60934
+
+
+class ConvertMilesToKilometresApp(App):
+    """ConvertMilesToKilometresApp is a Kivy app for converting miles to kilometres."""
+    def build(self):
+        Window.size = (400, 200)
+        self.title = "Convert Miles to Kilometres"
+        self.root = Builder.load_file('converting.kv')
+        return self.root
+
+    def handle_convert(self, value):
+        validate
+        result = value * MILES_TO_KM
+        self.root.ids.output_label.text = "{:.2f}".format(result)
+
+    def handle_increment(self, value, increment):
+        new_value = float(value) + increment
+        self.root.ids.input_number.text = str(new_value)
+
+    def validate_input_value(self, value):
+        try:
+            validated_value = float(value)
+            return validated_value
+        except ValueError:
+            return 0
+
+
+ConvertMilesToKilometresApp().run()
