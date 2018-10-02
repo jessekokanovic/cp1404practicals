200a8ba8e1cc0c25eeb76668d64f6b6a1ac2463d
jessekokanovic <41411258+jessekokanovic@users.noreply.github.com>
2018-10-02 14:57:03 +1000

    Add unreliable car class.

---
+++ prac_08/taxi_test.py
@@ -0,0 +1,11 @@
+from prac_08.taxi import Taxi
+
+prius = Taxi("Prius 1", 100)
+prius.drive(40)
+print(prius)
+prius.start_fare()
+prius.drive(100)
+print(prius)
+
+
+
---
+++ prac_08/unreliable_car_test.py
@@ -0,0 +1,5 @@
+from prac_08.unreliable_car import UnreliableCar
+
+car = UnreliableCar("Amarok", 100, 50)
+car.drive(40)
+print(car)
