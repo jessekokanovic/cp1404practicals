ad64f968bf57a572be8589f3b0ce89840bbc1544
jessekokanovic <41411258+jessekokanovic@users.noreply.github.com>
2018-10-22 23:05:28 +1000

    Fix issues with two capitals in a row.

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -11,28 +11,30 @@ def main():
     print("Starting directory is: {}".format(os.getcwd()))
     os.chdir('Lyrics/Christmas')
     print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
-    get_fixed_filename("The FirstNoel(remix).TXT")
+    print(get_fixed_filename("The firstNoel(remix).TXT"))
     # for filename in os.listdir('.'):
-    #     get_fixed_filename(filename)
+    #     print(get_fixed_filename(filename))
 
 
 def get_fixed_filename(filename):
     """Return a 'fixed' version of filename."""
     new_name = ""
+    filename = filename.replace(".TXT", ".txt")
     for index, current_letter in enumerate(filename):
         try:
             next_letter = filename[index + 1]
+            previous_letter = filename[index - 1]
         except IndexError:
             pass
-        if current_letter.islower() and next_letter.isupper():
+        if (current_letter.islower() and next_letter.isupper()) or (current_letter.isupper() and next_letter.isupper()):
             new_name = new_name + current_letter + "_"
-        elif current_letter in SPECIAL_CHARACTERS:
+        elif current_letter in SPECIAL_CHARACTERS and previous_letter.isalnum():
             new_name = new_name + "_" + current_letter
         elif current_letter.isspace():
             new_name = new_name + "_"
+            print(filename[index + 1].upper())
         else:
             new_name = new_name + current_letter
-    new_name = new_name.replace(".TXT", ".txt")
     return new_name
 
 
