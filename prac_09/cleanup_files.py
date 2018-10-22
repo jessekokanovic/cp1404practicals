5dfb64029ebc26e1e7c2e664914fc8617afce2ec
jessekokanovic <41411258+jessekokanovic@users.noreply.github.com>
2018-10-23 08:35:59 +1000

    Add renaming loop using os.walk()

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -8,12 +8,18 @@ SPECIAL_CHARACTERS = ['(']
 
 
 def main():
-    print("Starting directory is: {}".format(os.getcwd()))
-    os.chdir('Lyrics/Christmas')
-    print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
-    # print(get_fixed_filename("The firstNoel(remix).TXT"))
-    for filename in os.listdir('.'):
-        print(get_fixed_filename(filename))
+    # Process all directories
+    for directory_name, subdirectories, filenames in os.walk('.'):
+        print("Directory:", directory_name)
+        print("\tcontains subdirectories:", subdirectories)
+        print("\tand files:", filenames)
+        print("(Current working directory is: {})".format(os.getcwd()))
+
+        # Rename the files using the get_fixed_filename function
+        for filename in filenames:
+            current_file = os.path.join(directory_name, filename)
+            new_name = get_fixed_filename(current_file)
+            os.rename(current_file, new_name)
 
 
 def get_fixed_filename(filename):
