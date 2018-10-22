ec351f545484b7245e73411e89f515da135b0087
jessekokanovic <41411258+jessekokanovic@users.noreply.github.com>
2018-10-23 09:40:44 +1000

    Add version 1 of sort files.

---
+++ prac_09/sort_files.py
@@ -0,0 +1,34 @@
+import shutil
+import os
+
+
+# VERSION 1
+# Get to correct directory
+os.chdir('FilesToSort')
+
+# Find all the file extensions we need and store in a list
+extensions = []
+for file in os.listdir('.'):
+    try:
+        file_extension = file.split(".")[1]
+    except IndexError:
+        pass
+    if file_extension not in extensions:
+        extensions.append(file_extension)
+
+# Make new directories based on these extensions
+for extension in extensions:
+    try:
+        os.mkdir(extension)
+    except FileExistsError:
+        pass
+
+# Move files to correct directory
+for filename in os.listdir('.'):
+    if os.path.isdir(filename):
+        continue
+    else:
+        for extension in extensions:
+            if filename.endswith(extension):
+                shutil.move(filename, extension)
+
