59e411728e4a8ebfc97b3ac1cae50e1df5770326
jessekokanovic <41411258+jessekokanovic@users.noreply.github.com>
2018-10-22 22:42:52 +1000

    Try intermediate by filling an empty string one character at a time.

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -6,36 +6,34 @@ import os
 
 SPECIAL_CHARACTERS = ['(']
 
+
 def main():
     print("Starting directory is: {}".format(os.getcwd()))
     os.chdir('Lyrics/Christmas')
     print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
-    get_fixed_filename("TheFirstNoel.TXT")
+    get_fixed_filename("The FirstNoel(remix).TXT")
     # for filename in os.listdir('.'):
     #     get_fixed_filename(filename)
 
 
 def get_fixed_filename(filename):
     """Return a 'fixed' version of filename."""
-    new_name =""
+    new_name = ""
     for index, current_letter in enumerate(filename):
         try:
             next_letter = filename[index + 1]
         except IndexError:
             pass
-        print(current_letter)
-        print(next_letter)
         if current_letter.islower() and next_letter.isupper():
-            # new_name = new_name[:index + 1] + "_" + new_name[index + 1:]
             new_name = new_name + current_letter + "_"
-            print(new_name)
         elif current_letter in SPECIAL_CHARACTERS:
-            new_name = new_name[:index] + "_" + new_name[index:]
+            new_name = new_name + "_" + current_letter
         elif current_letter.isspace():
-            new_name = new_name[:index + 1].replace(" ", "_") + new_name[index + 1:]
+            new_name = new_name + "_"
+        else:
+            new_name = new_name + current_letter
     new_name = new_name.replace(".TXT", ".txt")
-
-    return print(new_name)
+    return new_name
 
 
 main()
